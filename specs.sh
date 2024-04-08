4c78c50396eda67ab8014ee519b9fd0d5b127297
Kyle Harding <kyle@balena.io>
2024-04-08 10:39:56 -0400

    initial commit
    Signed-off-by: Kyle Harding <kyle@balena.io>

---
+++ specs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo Information on main disk
+df -h /
+
+echo Memory info
+free -h
+
+echo Total CPUs:
+echo CPUs: $(nproc)
+
+echo CPU Model
+cat /proc/cpuinfo |grep -i "Model"|head -n 2
+
+echo Kernel and OS info
+uname -a
+
+echo Generally, KVM should not be available unless specifically enabled
+if ! [ -e /dev/kvm ]; then
+    echo "/dev/kvm does not exist"
+else
+    echo "/dev/kvm exists"
+fi
+
+echo OS
+cat /etc/os-release
+
+echo Egress IP:
+curl -s -L -S https://checkip.amazonaws.com
+
+echo Speed test of Internet
+sudo pip install speedtest-cli
+speedtest-cli
+
+echo Checking Docker
+docker run alpine:latest cat /etc/os-release
